ef10c3d773543356da2fc0d712cf2f41ae930954
maitrecanard <mathieusiaudeau@gmail.com>
2025-09-27 18:46:35 +0200

    add route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ContactApiController;
+use App\Http\Controllers\Api\VisitorsApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('/send-mail', [ContactApiController::class, 'sendMessage']);
+Route::post('/count-visitor', [VisitorsApiController::class, 'countVisitor']);
